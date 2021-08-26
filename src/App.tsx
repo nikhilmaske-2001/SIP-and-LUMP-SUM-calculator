7dcbcc6cc147298cc1321687662eea0c9e2b2c37
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-26 13:41:47 +0530

    Add a logic for calculating the total invested, return amount.

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,25 @@ function App() {
 
   const [formData, setFormData] = useState(initialState);
   const [totalAmount, setTotalAmount] = useState(0);
+  const [investedAmount, setInvestedAmount] = useState(0);
 
   const calculateTotal = () => {
-    var MI: number = +formData.monthly_investment;
-    var per: number = +formData.period;
-    var ret: number = +formData.return;
-    setTotalAmount(MI + per + ret);
+    var monthly_investment: number = +formData.monthly_investment;
+    var period: number = +formData.period;
+    var expected_return: number = +formData.return;
+    var investment_amount: number = 0;
+    var total: number = 0;
+
+    for (var year = 1; year <= period; year++) {
+      for (var month = 1; month <= 12; month++) {
+        total = total + monthly_investment;
+        investment_amount = investment_amount + monthly_investment;
+      }
+      var one_year_return = (total / 100) * expected_return;
+      total = total + one_year_return;
+    }
+    setInvestedAmount(Math.round(investment_amount));
+    setTotalAmount(total);
   };
 
   return (
@@ -42,7 +55,8 @@ function App() {
 
           <Button className={classes.Button} variant="outlined" color="primary" fullWidth
             onClick={calculateTotal}> Calculate </Button>
-          <Box className={classes.Total}>Total: {totalAmount}</Box>
+          <Box className={classes.Total}>Total: {totalAmount} <br />
+            Invested Amount : {investedAmount}</Box>
         </CardContent>
       </Card>
     </div >
