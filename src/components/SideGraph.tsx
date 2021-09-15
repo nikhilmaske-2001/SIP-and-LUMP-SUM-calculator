af9605529bd2e22193e1fb1933a6cf56f1e1a6f1
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-09-15 18:38:17 +0530

    Graph is working (bug present).

--- src/components/SideGraph.tsx
+++ src/components/SideGraph.tsx
@@ -1,125 +1,22 @@
-import { Card } from '@material-ui/core';
 import React from 'react';
+import { Card } from '@material-ui/core';
 import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import { useStyles } from './styles';
 
-const data = [
-    {
-        "name": "2021",
-        "invested": 4000,
-        "componded": 2400,
-        "amt": 2400
-    },
-    {
-        "name": "2022",
-        "invested": 3000,
-        "componded": 1398,
-        "amt": 2210
-    },
-    {
-        "name": "2023",
-        "invested": 2000,
-        "componded": 9800,
-        "amt": 2290
-    },
-    {
-        "name": "2024",
-        "invested": 2780,
-        "componded": 3908,
-        "amt": 2000
-    },
-    {
-        "name": "2025",
-        "invested": 1890,
-        "componded": 4800,
-        "amt": 2181
-    },
-    {
-        "name": "2026",
-        "invested": 2390,
-        "componded": 3800,
-        "amt": 2500
-    },
-    {
-        "name": "2027",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    },
-    {
-        "name": "2028",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    },
-    {
-        "name": "2029",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    },
-    {
-        "name": "2030",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    },
-    {
-        "name": "2031",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    },
-    {
-        "name": "2032",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    },
-    {
-        "name": "2033",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    },
-    {
-        "name": "2034",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    },
-    {
-        "name": "2035",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    },
-    {
-        "name": "2036",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    },
-    {
-        "name": "2037",
-        "invested": 3490,
-        "componded": 4300,
-        "amt": 2100
-    }
-]
+const data = [{}];
 
-function SideGraph({ investedGraphData, expectedGraphData }: any) {
+function SideGraph({ investedData, compondedData }: any) {
     const classes = useStyles();
-    const invested_graph = investedGraphData;
-    const componded_graph = expectedGraphData;
-    const period = investedGraphData.length;
+    const invested_graph = investedData;
+    const componded_graph = compondedData;
+    const period = investedData.length;
 
     for (var year = 1; year <= period; year++) {
         var obj = {
             "name": year.toString(),
             "invested": invested_graph[year],
             "componded": componded_graph[year],
-            "amt": 2020,
+            "amt": 2000 + year,
         };
         data.push(obj);
     };
